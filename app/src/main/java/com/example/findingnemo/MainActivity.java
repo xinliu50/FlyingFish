2571ea67e097d9aa530bc8b548561903ddad59d3
Xin Liu <xinliu253@yahoo.com>
2019-11-24 17:31:49 -0500

    adding fish

--- app/src/main/java/com/example/findingnemo/MainActivity.java
+++ app/src/main/java/com/example/findingnemo/MainActivity.java
@@ -5,10 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private FlyingFishView gameView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        gameView = new FlyingFishView(this);
+        setContentView(gameView);
     }
 }
